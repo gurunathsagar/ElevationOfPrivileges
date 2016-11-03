45eb888d51cea22e0fceb820abb8e2e5d17ad968
Gurunath Hanamsagar <gurunath.sagar@gmail.com>
2016-11-03 21:08:49 +0000

    Added Mux installation command to shell script.

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Installing go
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:duh/golang
+sudo apt-get update
+sudo apt-get install golang
+#go version
+#go version go1.1.1 linux/amd64
+
+export GOROOT=/usr/lib/go
+export GOBIN=/usr/bin/go
+
+mkdir src
+cd src
+
+# Install Mux. This should be run in workspace/src
+go get -u github.com/gorilla/mux
